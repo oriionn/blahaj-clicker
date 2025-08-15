b0d083bc3f0e6a478701f2d0c50e86887ed3edfc
Orion / Emilien <contact@oriondev.fr>
2025-08-15 12:39:11 +0200

    add anti spam ip

--- src/websocket.ts
+++ src/websocket.ts
@@ -7,22 +7,25 @@ let blahajInThreeSecondsPerUsers: Map<string, number> = new Map();
 export default new Elysia({ prefix: "/websocket", websocket: { idleTimeout: 20 * 60 } })
     .ws("/", {
         open(ws) {
+            let id = ws.data.headers["CF-Connecting-IP"] ?? ws.id;
+
             ws.subscribe("blahaj")
             ws.send(`blahaj_${getCount()}`);
-            blahajInThreeSecondsPerUsers.set(ws.id, 0);
+            blahajInThreeSecondsPerUsers.set(id, 0);
         },
         message(ws, body) {
+            let id = ws.data.headers["CF-Connecting-IP"] ?? ws.id;
             if (body === "ping") {
                 return ws.send("pong");
             }
 
             // Anti spamhaj (Patent n°f3mb0y)
             blahajInThreeSeconds++;
-            let userBlahaj = (blahajInThreeSecondsPerUsers.get(ws.id) ?? 0) + 1
-            blahajInThreeSecondsPerUsers.set(ws.id, userBlahaj)
+            let userBlahaj = (blahajInThreeSecondsPerUsers.get(id) ?? 0) + 1
+            blahajInThreeSecondsPerUsers.set(id, userBlahaj)
             setTimeout(() => {
                 blahajInThreeSeconds--;
-                blahajInThreeSecondsPerUsers.set(ws.id, (blahajInThreeSecondsPerUsers.get(ws.id) ?? 0) - 1)
+                blahajInThreeSecondsPerUsers.set(id, (blahajInThreeSecondsPerUsers.get(id) ?? 0) - 1)
             }, 3000);
             if (blahajInThreeSeconds >= 200 || userBlahaj >= 60) {
                 ws.publish("blahaj", "spamhaj")
