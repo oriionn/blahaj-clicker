df9741362735427ed64da8cf3579780262f9cff9
Orion / Emilien <contact@oriondev.fr>
2025-08-17 00:43:51 +0200

    Add goal + change the click handling system :3

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,8 +2,11 @@ const blahaj = document.getElementById("blahaj");
 const error = document.getElementById("error");
 const mode = document.getElementById("mode");
 const countEl = document.getElementById("count");
+const progress = document.getElementById("progress");
+const goal = 1000000;
 let init = true;
 let count = 0;
+let clickUUIDs = [];
 
 let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 if (localStorage.getItem("theme")) {
@@ -11,13 +14,69 @@ if (localStorage.getItem("theme")) {
 }
 updateTheme();
 
-const ws = new WebSocket("/websocket");
+let ws;
+
+function initWebsocket() {
+    let ws_temp = new WebSocket("/websocket");
+    ws_temp.addEventListener("message", (event) => {
+        if (init) {
+            error.textContent = "";
+        }
+
+        if (typeof event.data === "string") {
+            if (event.data.startsWith("blahaj_")) {
+                count = parseInt(event.data.split("_")[1]);
+                let uuid = event.data.split("_")[2];
+                if (!uuid) updateCount();
+                updateCount(uuid);
+            } else if (event.data === "spamhaj") {
+                error.textContent = `Blahaj spam has been detected, please wait for the spam to subside.`;
+                if (timeout) clearTimeout(timeout);
+                timeout = setTimeout(() => {
+                    error.textContent = "";
+                }, 1000);
+            } else if (event.data.startsWith("show_")) {
+                let urlEncoded = event.data.split("_")[1];
+                let url = atob(urlEncoded);
+                document.body.style.background = `url(${url})`;
+                document.body.style.backgroundSize = "cover";
+                document.body.style.backgroundRepeat = "no-repeat";
+                document.body.classList.add("dark");
+            }
+        }
+    });
+
+    ws_temp.addEventListener("close", (event) => {
+        let err = "Connection to websocket lost, attempt to reconnect to websocket...";
+        error.textContent = err;
+        console.log(err);
+        init = true;
+        ws = initWebsocket();
+    });
+
+    return ws_temp;
+}
+
+ws = initWebsocket();
 let timeout;
 
 setInterval(() => ws.send("ping"), 30 * 1000)
 
 blahaj.addEventListener("click", () => {
-    ws.send("click");
+    count++;
+    updateCount("1");
+
+    let uuid = crypto.randomUUID();
+    clickUUIDs.push(uuid);
+    fetch(`/websocket/click`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            uuid
+        })
+    });
 });
 
 mode.addEventListener("click", () => {
@@ -25,26 +84,21 @@ mode.addEventListener("click", () => {
     updateTheme();
 });
 
-ws.addEventListener("message", (event) => {
-    if (typeof event.data === "string") {
-        if (event.data.startsWith("blahaj_")) {
-            count = parseInt(event.data.split("_")[1]);
-            updateCount();
-        } else if (event.data === "spamhaj") {
-            error.textContent = `Blahaj spam has been detected, please wait for the spam to subside.`;
-            if (timeout) clearTimeout(timeout);
-            timeout = setTimeout(() => {
-                error.textContent = "";
-            }, 1000);
-        }
-    }
-});
+function updateCount(uuid = "0") {
+    countEl.textContent = count.toLocaleString("fr-FR");
 
-function updateCount() {
-    countEl.textContent = count;
+    if (uuid === "0") return;
+    if (clickUUIDs.includes(uuid)) {
+        clickUUIDs = clickUUIDs.filter(u => u !== uuid);
+        return;
+    }
 
     let degree = (count - Math.floor(count / 45) * 45) * 8;
     blahaj.querySelector("div").style.transform = `rotate(${degree}deg)`;
+
+    let percentage = Math.floor((count / goal) * 100);
+    if (percentage >= 100) percentage = 100;
+    progress.style.width = `${percentage}%`
 
     if (init) {
         init = false;
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,4 +1,4 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { getCount, updateCount } from "./utils/database";
 
 let blahajInThreeSeconds = 0;
@@ -10,35 +10,48 @@ export default new Elysia({ prefix: "/websocket", websocket: { idleTimeout: 20 *
             let id = ws.data.headers["CF-Connecting-IP"] ?? ws.id;
 
             ws.subscribe("blahaj")
-            ws.send(`blahaj_${getCount()}`);
+            ws.send(`blahaj_${getCount()}_1`);
             blahajInThreeSecondsPerUsers.set(id, 0);
+
+            if (isGoalReached()) return ws.send(`show_${btoa(process.env.GOAL_IMAGE_URL!)}`);
         },
-        message(ws, body) {
-            let id = ws.data.headers["CF-Connecting-IP"] ?? ws.id;
-            if (body === "ping") {
-                return ws.send("pong");
-            }
-
-            // Anti spamhaj (Patent n°f3mb0y)
-            blahajInThreeSeconds++;
-            let userBlahaj = (blahajInThreeSecondsPerUsers.get(id) ?? 0) + 1
-            blahajInThreeSecondsPerUsers.set(id, userBlahaj)
-            setTimeout(() => {
-                blahajInThreeSeconds--;
-                blahajInThreeSecondsPerUsers.set(id, (blahajInThreeSecondsPerUsers.get(id) ?? 0) - 1)
-            }, 3000);
-            if (blahajInThreeSeconds >= 200 || userBlahaj >= 60) {
-                ws.publish("blahaj", "spamhaj")
-                return ws.send("spamhaj")
-            }
-
-            let count = getCount() + 1;
-            updateCount(count);
-            let payload = `blahaj_${count}`;
-            ws.publish("blahaj", payload);
-            ws.send(payload);
+        message(ws) {
+            ws.send("pong");
         },
         close(ws) {
             ws.unsubscribe("blahaj");
         }
     })
+    .post("/click", async ({ headers, server, body }) => {
+        // Anti spamhaj (Patent n°f3mb0y)
+        let ip = headers["CF-Connecting-IP"];
+        blahajInThreeSeconds++;
+        let userBlahaj = 0;
+        if (ip) userBlahaj = (blahajInThreeSecondsPerUsers.get(ip) ?? 0) + 1;
+
+        setTimeout(() => {
+            blahajInThreeSeconds--;
+            if (ip) blahajInThreeSecondsPerUsers.set(ip, (blahajInThreeSecondsPerUsers.get(ip) ?? 0) - 1)
+        }, 3000);
+
+        if (blahajInThreeSeconds >= 160 || userBlahaj >= 60) {
+            server!.publish("blahaj", `spamhaj`);
+        }
+
+        let count = getCount() + 1;
+        updateCount(count);
+        server!.publish("blahaj", `blahaj_${count}_${body.uuid}`);
+
+        if (isGoalReached()) return server!.publish("blahaj", `show_${btoa(process.env.GOAL_IMAGE_URL!)}`);
+
+        return { ok: true }
+    }, {
+        body: t.Object({
+            uuid: t.String()
+        })
+    })
+
+function isGoalReached(): boolean {
+    let count = getCount();
+    return count >= Number(process.env.GOAL!);
+}
